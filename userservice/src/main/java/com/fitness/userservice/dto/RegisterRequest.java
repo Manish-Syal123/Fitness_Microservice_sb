0cf15b55528d26fc3db29ad4b73ee9edf409b872
Manish_Syal <manishsyal77@gmail.com>
2025-06-16 16:47:43 +0530

    Integerated/inserted/Accommodated keycloak ID into UserService

--- userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
+++ userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -15,6 +15,8 @@ public class RegisterRequest {
     @Size(min = 6, message = "Password must have atlest 6 characters")
     private String password;
 
+    private String keycloakId;
+
     private String firstName;
     private String lastName;
 }
