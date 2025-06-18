427fe3be5263e406d94e9121b743a1f4fc645ab1
Manish_Syal <manishsyal77@gmail.com>
2025-06-18 17:30:16 +0530

    Built the ActivityForm on the dashboard frontend

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import { useContext, useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router";
 import { Button } from "@/components/ui/button";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "./store/authSlice";
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
+
+const ActivitiesPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <ActivityForm onActivitiesAdded={() => window.location.reload()} />
+      <ActivityList />
+    </div>
+  );
+};
 
 function App() {
   const { token, tokenData, logIn, logOut, isAuthenticated } =
@@ -20,7 +34,7 @@ function App() {
 
   return (
     <Router>
-      {!token && !authReady ? (
+      {!token ? (
         <div className="flex items-center justify-center h-screen">
           <div className="text-center">
             <h1 className="text-2xl font-bold mb-4">Welcome to the App</h1>
@@ -42,30 +56,46 @@ function App() {
           </div>
         </div>
       ) : (
-        <div className="flex items-center justify-center">
-          <div className="text-center">
-            <h1 className="text-2xl font-bold mb-4">Welcome Back!</h1>
-            <p className="mb-4">
-              You are logged in as {tokenData?.name || "User"}.
-            </p>
-            <Button
-              className="cursor-pointer"
-              onClick={() => {
-                logOut()
-                  .then(() => {
-                    setAuthReady(false);
-                  })
-                  .catch((error) => {
-                    console.error("Logout failed:", error);
-                  });
-              }}
-            >
-              Logout
-            </Button>
-            <div className="mt-4 p-4 bg-gray-100 rounded overflow-y-scroll max-h-96 border-2 border-gray-300">
-              <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-            </div>
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <Alert className="mb-4 flex items-center shadow-lg">
+              <CheckCircle2Icon />
+              <AlertTitle>
+                Success! You are logged in as "{tokenData?.name || "User"}"
+              </AlertTitle>
+              <AlertDescription className="ml-30">
+                <Button
+                  className="cursor-pointer"
+                  onClick={() => {
+                    logOut()
+                      .then(() => {
+                        setAuthReady(false);
+                      })
+                      .catch((error) => {
+                        console.error("Logout failed:", error);
+                      });
+                  }}
+                >
+                  Logout
+                </Button>
+              </AlertDescription>
+            </Alert>
           </div>
+
+          <Routes>
+            <Route
+              path="/"
+              element={
+                token ? (
+                  <Navigate to={"/activities"} replace />
+                ) : (
+                  <div>Welcome Please Login</div>
+                )
+              }
+            />
+            <Route path="/activities" element={<ActivitiesPage />} />
+            <Route path="/activities/:id" element={<ActivityDetail />} />
+          </Routes>
         </div>
       )}
     </Router>
--- fitness-app-frontend/src/components/ActivityForm.jsx
+++ fitness-app-frontend/src/components/ActivityForm.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-const ActivityForm = () => {
-  return <div>ActivityForm</div>;
+const ActivityForm = ({ onActivityAdded }) => {
+  const [activity, setActivity] = React.useState({
+    type: "RUNNING",
+    duration: "",
+    caloriesBurned: "",
+    additionalMetrics: {},
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // await addActivity(activity);
+      onActivityAdded();
+      setActivity({ type: "RUNNING", duration: "", caloriesBurned: "" });
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto p-4 bg-white shadow-md rounded-lg border hover:shadow-lg transition-shadow">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <Label htmlFor="text">Activity Type</Label>
+        <Select
+          value={activity.type}
+          onValueChange={(value) => setActivity({ ...activity, type: value })}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Types</SelectLabel>
+              <SelectItem value="RUNNING">RUNNING</SelectItem>
+              <SelectItem value="CYCLING">CYCLING</SelectItem>
+              <SelectItem value="SWIMMING">SWIMMING</SelectItem>
+              <SelectItem value="HIKING">HIKING</SelectItem>
+              <SelectItem value="YOGA">YOGA</SelectItem>
+              <SelectItem value="WEIGHTLIFTING">WEIGHTLIFTING</SelectItem>
+              <SelectItem value="WALKING">WALKING</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Label htmlFor="duration">Duration (minutes)</Label>
+        <Input
+          id="duration"
+          type="number"
+          placeholder="Enter duration in minutes"
+          value={activity.duration}
+          onChange={(e) =>
+            setActivity({ ...activity, duration: e.target.value })
+          }
+          required
+        />
+        <Label htmlFor="caloriesBurned">Calories Burned</Label>
+        <Input
+          id="caloriesBurned"
+          type="number"
+          placeholder="Enter calories burned"
+          value={activity.caloriesBurned}
+          onChange={(e) =>
+            setActivity({ ...activity, caloriesBurned: e.target.value })
+          }
+          required
+        />
+        <Button
+          type="submit"
+          className="w-full py-2 rounded hover:bg-green-600 transition-colors cursor-pointer"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default ActivityForm;
